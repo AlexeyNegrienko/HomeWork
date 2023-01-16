e8009a3c329d06ddcbaa3bbb22f81242474b84ea
Алексей <kagan5@mail.ru>
2023-01-16 21:25:18 +0300

    Чет/Нечет

---
+++ work03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double x = Convert.ToDouble(Console.ReadLine());                            
+ if (x%2==0)
+{
+    Console.WriteLine("число четное");
+ }
+    else
+ {
+     Console.WriteLine("Число нечетное");
+}
