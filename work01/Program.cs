5482c4615ad2d0f9d0eee6fd02090dbc56ccbd27
Алексей <kagan5@mail.ru>
2023-01-16 19:29:13 +0300

    Максимальное из 3 чисел

---
+++ work01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int max=x;
+if (y>max)
+    max=y;
+if (z>max)
+    max=z;
+  Console.WriteLine($"{max}");   
+    
