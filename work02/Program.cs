cfe442970f6cbd85061819657e3ff9ac30a93f27
Алексей <kagan5@mail.ru>
2023-01-16 20:30:27 +0300

    Максимальное и минимальное

---
+++ work02/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+    int max=x;
+    int min=y;
+if (x>y) 
+{
+Console.WriteLine($"максимальное число: {max}");   
+Console.WriteLine($"минимальное число: {min}");
+} 
+if (x<y)
+{
+    max=y;
+    min=x;
+Console.WriteLine($"максимальное число: {max}");   
+Console.WriteLine($"минимальное число: {min}");
+}
+if (x==y)
+    Console.WriteLine($"числа равны");
