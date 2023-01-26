708b1fd6a137ba61822572a28fc3b9cc9348ff5a
Алексей <kagan5@mail.ru>
2023-01-26 11:23:34 +0300

    Доп.Задание. Сбор черники

---
+++ work34/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+Console.Write("Введите число кустов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < n; i++)
+array[i] = Convert.ToInt32(Console.ReadLine());
+
+int maxS = 0;
+for (int i = 1; i < array.Length - 1; i++)
+{
+int sum = array[i - 1] + array[i] + array[i + 1];
+if (sum > maxS)
+maxS = sum;
+}
+if (array[0] + array[1] + array[array.Length - 1] > maxS)
+maxS = array[0] + array[1] + array[array.Length - 1];
+
+if (array[array.Length - 1] + array[array.Length - 2] + array[0] > maxS)
+maxS = array[array.Length - 1] + array[array.Length - 2] + array[0];
+
+Console.WriteLine(maxS);
