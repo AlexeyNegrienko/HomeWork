965c4e86cc24fb63c6978cd5d55fdb0032ced709
Алексей <kagan5@mail.ru>
2023-01-27 08:53:44 +0300

    Задача 25.Возводит А в степень Б

---
+++ work41/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возводит a в степень b
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine()),x=1;
+Console.Write("Введите 2-ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= b; i++)
+{
+x = x * a;
+}
+Console.WriteLine(x);
