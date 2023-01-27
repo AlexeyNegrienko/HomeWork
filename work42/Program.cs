3be54c941aa61e1770098c401443c4296f5c548c
Алексей <kagan5@mail.ru>
2023-01-27 08:58:03 +0300

    Задача 27.Сумма цифр в числе

---
+++ work42/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сумма цифр в числе
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x=0;
+while (n > 0)
+{
+x=x+n%10;
+n=n/10;
+}
+Console.WriteLine(x);
