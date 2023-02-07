76e8da40f8ab79886de8713f2649b4861a3baee0
Алексей <kagan5@mail.ru>
2023-02-07 03:15:49 +0300

    Задача 41.Числа больше 0

---
+++ work61/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 41:Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+Console.Write("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m > 0)
+count++; 
+}
+Console.WriteLine($"Количество чисел больше 0 равно: {count}");
