6145cf54e9c81d05983110ba12c5421e3f7241fd
Алексей <kagan5@mail.ru>
2023-02-07 03:41:25 +0300

    Задача 43.Точка пересечения.

--- work62/Program.cs
+++ work62/Program.cs
@@ -2,15 +2,15 @@
 // // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // // значения b1, k1, b2 и k2 задаются пользователем.
 
-// Console.Write("Введите k1: ");
-// double k1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите b1: ");
-// double b1 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите k2: ");
-// double k2 = Convert.ToDouble(Console.ReadLine());
-// Console.Write("Введите b2: ");
-// double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
  
-// double x = (b2 - b1) / (k1 - k2);
-// double y = k1 * x + b1;
-// Console.WriteLine($"Пересечение в точке: ({x};{y})");
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
